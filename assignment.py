28de14a10561aaa7d44ffd31cea8d8817c4cb35a
Sam Hinshaw <samuel.hinshaw@gmail.com>
2018-07-17 11:40:56 -0700

    Start working on assignment class

--- assignment.py
+++ assignment.py
@@ -8,19 +8,29 @@ class Assignment:
   Assignment object for manipulating Canvas assignments
   """
 
-  def __init__(self, course_id=None, assignment_id=None, name=None):
+  def __init__(self, course_id, canvas_url, assignment_id, name):
+    """
+    Users must specify a course ID, but can specify either an assignment name or
+    assignment ID.
+
+    :param course_id: The (numeric) Canvas Course ID. 
+    :param canvas_url: Base URL to your Canvas deployment. Probably something like "canvas.institution.edu".
+    :param token_env_name: The name of your Canvas Token environment variable. 
+
+    :returns: A Course object for performing operations on an entire course at once.
+    """
     self.course_id = course_id
     self.name = name
     if assignment_id:
       self.assignment_id = assignment_id
     else:
-      self.assignment_id = self._findCourse(name)
+      self.assignment_id = self._find_course(name)
 
-  def changeCourseURL(self, url):
+  def change_course_url(self, url):
     # requests.get()
     print('URL Updated')
 
-  def _findCourse(self, name):
+  def _find_course(self, name):
     # Here, match the course by the name if no ID supplied
     print('Here is the assignment ID')
     return (1)
