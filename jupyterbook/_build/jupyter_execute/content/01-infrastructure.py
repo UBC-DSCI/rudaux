396b80d543ddfb2df26a0244c82b11a824a9afad
ttimbers <tiffany.timbers@gmail.com>
2020-06-19 12:42:03 -0700

    first working skeleton of the book

---
+++ jupyterbook/_build/jupyter_execute/content/01-infrastructure.py
@@ -0,0 +1,14 @@
+# Course Infrastructure
+
+### Inventory list
+- Canvas course shell
+- Two JupyterHub servers (each running on an AWS EC2 instance)
+- Rudaux to build the JupyterHubs and connect the above
+
+## Building JupyterHub servers
+
+### Installation setup
+1. Install Ansible on your local computer following these directions: https://docs.ansible.com/ansible/latest/installation_guide/intro_installation.html#installation-guide
+
+
+
