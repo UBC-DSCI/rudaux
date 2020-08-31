ad7f58942a4f96ab30dc6e488c36540aa779ec62
Trevor Campbell <trevor.d.campbell@gmail.com>
2020-08-31 15:17:11 -0700

    moved rudaux archive; added vim tmp files to gitig

--- rudaux/course.py
+++ rudaux/course.py
@@ -1,36 +1,166 @@
-import posixpath
-import urllib.parse
-import requests
-import datetime
-import json
+#import posixpath
+#import urllib.parse
+#import requests
+#import datetime
+#import json
 
-import Student
-import Assignment
+import os
+from tqdm import tqdm
+from git import Repo
+from traitlets.config import Config
+from traitlets.config.application import Application
 
-#Use Traitlet
-from traitlets.config.configurable import Configurable, SingletonConfigurable
-from traitlets import Int, Float, Unicode, Bool, Bytes
-from traitlets.config import get_config
+class Course:
+    """
+    Course object for managing an entire Canvas/JupyterHub/nbgrader course.
+    """
 
+    def __init__(self, course_dir=None, auto=False):
+    """Initialize a course from a config file. 
+    :param course_dir: The directory your course. If none, defaults to current working directory. 
+    :type course_dir: str
+    :param auto: Suppress all prompts, automatically answering yes.
+    :type auto: bool
 
-#load custom traits for traitlets
-import sys
-sys.path.append('../traitlet_types')
-from traitlet_types.Course_id import Course_id
-from traitlet_types.Hostname import Hostname
+    :returns: A Course object for performing operations on an entire course at once.
+    :rtype: Course
+    """
 
-#import configuration for traitlets
-#import base_config
-#c = get_config()
+    #=======================================#
+    #     Working Directory & Git Sync      #
+    #=======================================#
+
+    # Set up the working directory. If no course_dir has been specified, then it
+    # is assumed that this is the course directory. 
+    self.working_directory = course_dir if course_dir is not None else os.getcwd()
     
-    
-#TODO Cache Everything
-#TODO Replicate All Functions in canvas.py
-#TODO Add error checking
+    repo = Repo(self.working_directory)
 
-class Course(Configurable):# QUESTION: or perhaps SingletonConfigurable?
-    #1. Const for the course
-    #Course Configuration Class Attributes
+    # Before we do ANYTHING, make sure our working directory is clean with no
+    # untracked files! Unless we're running a automated job, in which case we
+    # don't want to fail for an unexpected reason.
+    if (repo.is_dirty() or repo.untracked_files) and (not auto):
+      continue_with_dirty = input(
+        """
+        Your repository is currently in a dirty state (modifications or
+        untracked changes are present). We strongly suggest that you resolve
+        these before proceeding. Continue? [y/n]:"""
+      )
+      # if they didn't say no, exit
+      if continue_with_dirty.lower() != 'y':
+        sys.exit("Exiting...")
+
+    # PRINT BANNER
+    print(AsciiTable([['Initializing Course and Pulling Instructors Repo']]).table)
+
+    # pull the latest copy of the repo
+    utils.pull_repo(repo_dir=self.working_directory)
+
+    # Make sure we're running our nbgrader commands within our instructors repo.
+    # this will contain our gradebook database, our source directory, and other
+    # things.
+    config = Config()
+    config.CourseDirectory.root = self.working_directory
+
+    #=======================================#
+    #              Load Config              #
+    #=======================================#
+
+    # Check for an nbgrader config file...
+    if not os.path.exists(os.path.join(self.working_directory, 'nbgrader_config.py')):
+      # if there isn't one, make sure there's at least a rudaux config file
+      if not os.path.exists(os.path.join(self.working_directory, 'rudaux_config.py')):
+        sys.exit(
+          """
+          You do not have nbgrader_config.py or rudaux_config.py in your current
+          directory. We need at least one of these to set up your course
+          parameters! You can specify a directory with the course_dir argument
+          if you wish.
+          """
+        )
+
+    # use the traitlets Application class directly to load nbgrader config file.
+    # reference:
+    # https://github.com/jupyter/nbgrader/blob/41f52873c690af716c796a6003d861e493d45fea/nbgrader/server_extensions/validate_assignment/handlers.py#L35-L37
+
+    # ._load_config_files() returns a generator, so if the config is missing,
+    # the generator will act similarly to an empty array
+
+    # load rudaux_config if it exists, otherwise just bring in nbgrader_config.
+    for rudaux_config in Application._load_config_files('rudaux_config', path=self.working_directory):
+      config.merge(rudaux_config)
+    
+    for nbgrader_config in Application._load_config_files('nbgrader_config', path=self.working_directory):
+      config.merge(nbgrader_config)
+
+    #=======================================#
+    #           Set Config Params           #
+    #=======================================#
+
+    ## NBGRADER PARAMS
+
+    # If the user set the exchange, perform home user expansion if necessary
+    if config.get('Exchange', {}).get('root') is not None:
+      # perform home user expansion. Should not throw an error, but may
+      try:
+        # expand home user in-place
+        config['Exchange']['root'] = os.path.expanduser(config['Exchange']['root'])
+      except:
+        pass
+
+    ## CANVAS PARAMS
+
+    # Before we continue, make sure we have all of the necessary parameters.
+    self.course_id = config.get('Canvas', {}).get('course_id')
+    self.canvas_url = config.get('Canvas', {}).get('canvas_url')
+    self.external_tool_name = config.get('Canvas', {}).get('external_tool_name')
+    self.external_tool_level = config.get('Canvas', {}).get('external_tool_level')
+    # The canvas url should have no trailing slash
+    self.canvas_url = re.sub(r"/$", "", self.canvas_url)
+
+    ## GITHUB PARAMS
+    self.stu_repo_url = config.get('GitHub', {}).get('stu_repo_url', '')
+    self.assignment_release_path = config.get('GitHub', {}).get('assignment_release_path')
+    self.ins_repo_url = config.get('GitHub', {}).get('ins_repo_url')
+    # subpath not currently supported
+    # self.ins_dir_subpath = config.get('GitHub').get('ins_dir_subpath')
+      
+    ## JUPYTERHUB PARAMS
+
+    self.hub_url = config.get('JupyterHub', {}).get('hub_url')
+    # The hub url should have no trailing slash
+    self.hub_url = re.sub(r"/$", "", self.hub_url)
+    # Get Storage directory & type
+    self.storage_path = config.get('JupyterHub', {}).get('storage_path', )
+    self.zfs = config.get('JupyterHub', {}).get('zfs') # Optional, default is false!
+    self.zfs_regex = config.get('JupyterHub', {}).get('zfs_regex') # default is false!
+    self.zfs_datetime_pattern = config.get('JupyterHub', {}).get('zfs_datetime_pattern') # default is false!
+    # Note hub_prefix, not base_url, to avoid any ambiguity
+    self.hub_prefix = config.get('JupyterHub', {}).get('base_url')
+    # If prefix was set, make sure it has no trailing slash, but a preceding
+    # slash
+    if self.hub_prefix is not None:
+      self.hub_prefix = re.sub(r"/$", "", self.hub_prefix)
+      if re.search(r"^/", self.hub_prefix) is None:
+        self.hub_prefix = fr"/{self.hub_prefix}"
+
+    ## COURSE PARAMS
+
+    self.grading_image = config.get('Course', {}).get('grading_image')
+    self.tmp_dir = config.get('Course', {}).get('tmp_dir')
+    assignment_list = config.get('Course', {}).get('assignments')
+
+    self.course_timezone = config.get('Course', {}).get('timezone')
+    self.system_timezone = pendulum.now(tz='local').timezone.name
+
+    ## Repurpose the rest of the params for later batches
+    ## (Hang onto them in case we need something)
+
+    self._full_config = config
+
+
+        #1. Const for the course
+        #Course Configuration Class Attributes
         #TODO not able to use a seperate config file
         hostname = Unicode(u'https://instructure.ubc.ca')#.tag(config=True)
         course_id = Int(2039048)#.tag(config=True)
