398f9a9af4d636e82a9901caffe5c37e50dacc97
Trevor Campbell <trevor.d.campbell@gmail.com>
2020-09-03 14:05:11 -0700

    minor bugfix in pword validation in encrpyt_pw

--- rudaux/encrypt_password.py
+++ rudaux/encrypt_password.py
@@ -3,7 +3,7 @@ import hashlib
 import re
 
 #create salt and pw validator regex
-validator_regex = re.compile(r"^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$")
+validator_regex = re.compile(r"^(?=.*[A-Za-z])(?=.*[0-9]).{8,}$")
 salt = secrets.token_hex(64)
 
 print('---------------------------------------------------------------------')
