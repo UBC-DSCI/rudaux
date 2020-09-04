8e16dd09e1e011eda7646254d95ec9cfd2f404e1
Trevor Campbell <trevor.d.campbell@gmail.com>
2020-09-03 18:23:59 -0700

    minor ed to cmds ideas

--- rudaux/commands.py
+++ rudaux/commands.py
@@ -1,7 +1,12 @@
-status
-schedule #creates a schedule of commands that run automatically
-list
-add acct to marking server
-remove acct marking server
 
+status #return a report of status; subcommands:
+#assignment (graded / feedback / solutions / etc)
+#snapshot schedule 
+#hard drive / memory usage? 
+#errors in assignment grading
+
+schedule_tasks #creates a schedule of commands that run automatically
+               #snapshots, cloning repos, pulling, etc
+
+run #force running of a particular task now
 
