6976553acddab68505cf492adbb68d19765cef70
Trevor Campbell <trevor.d.campbell@gmail.com>
2020-09-03 17:34:43 -0700

    bugfix in init.py

--- dictauth/dictauth/__init__.py
+++ dictauth/dictauth/__init__.py
@@ -1,3 +1,3 @@
-from users import add_user, remove_user, list_users
-from encrypt_password import encrypt_password
-from dictionary_authenticator import DictionaryAuthenticator
+from .users import add_user, remove_user, list_users
+from .encrypt_password import encrypt_password
+from .dictionary_authenticator import DictionaryAuthenticator
