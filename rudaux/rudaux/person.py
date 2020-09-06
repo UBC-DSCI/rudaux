63945c720cedbe2059a7bbd695edbf89ff17e18c
Trevor Campbell <trevor.d.campbell@gmail.com>
2020-09-06 10:41:07 -0700

    moved rudaux config to tests folder

--- rudaux/rudaux/person.py
+++ rudaux/rudaux/person.py
@@ -3,19 +3,6 @@ from traitlets import Int, Float, Unicode, Bool
 
 class Person:
 
-    # Instance Attributes
-    def __init__(self):
-         #TODO attributes in a dictionary
-       
-        self.personal_info = {}
+    def __init__(self, canvasdict):
+        self.__dict__.update(canvasdict)
         self.submissions = []
-
-    # Getter Methods
-    def get_submissions(self):
-        #TODO
-        return 0 
-
-
-    #TODO
-    # Post grade?
-    
