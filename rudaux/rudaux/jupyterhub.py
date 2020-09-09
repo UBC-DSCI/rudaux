712fc5cc53198da3f7838d169f23d6bf0ed69ea2
Trevor Campbell <trevor.d.campbell@gmail.com>
2020-09-09 12:36:18 -0700

    removed useless error handling from jhub

--- rudaux/rudaux/jupyterhub.py
+++ rudaux/rudaux/jupyterhub.py
@@ -22,24 +22,14 @@ class JupyterHub(object):
     def snapshot_all(self, snap_name):
         cmd_list = ['zfs', 'snapshot', '-r', self.jupyterhub_user_folder_root + '@'+snap_name]
         if not self.dry_run:
-            try:
-                check_output(cmd_list)
-            except CalledProcessError as e:
-                print('Error taking snapshot')
-                print(e)
-                print(e.output)
+            check_output(cmd_list)
         else:
             print('[Dry run: would have called: ' + ' '.join(cmd_list) + ']')
 
     def snapshot_user(self, user, snap_name):
         cmd_list = ['zfs', 'snapshot', os.path.join(self.jupyterhub_user_folder_root, user).rstrip('/') + '@'+snap_name]
         if not self.dry_run:
-            try:
-                check_output(cmd_list)
-            except CalledProcessError as e:
-                print('Error taking snapshot')
-                print(e)
-                print(e.output) 
+            check_output(cmd_list)
         else:
             print('[Dry run: would have called: ' + ' '.join(cmd_list) + ']')
 
