3786e6c870adc033b44ccd860709291441bb7d05
Trevor Campbell <trevor.d.campbell@gmail.com>
2020-09-11 12:22:14 -0700

    implemented docker contaiuner pool

--- rudaux/rudaux/docker.py
+++ rudaux/rudaux/docker.py
@@ -5,17 +5,28 @@ class Docker(object):
     def __init__(self, course):
         self.client = docker.from_env()
         self.image = 'ubc-dsci/r-dsci-grading:v0.11.0'
-
-        self.jupyterhub_config_dir = course.config.jupyterhub_config_dir
-        self.jupyterhub_user_folder_root = course.config.jupyterhub_user_folder_root
-        self.assignment_folder_root = course.config.assignment_folder_root
         self.dry_run = course.dry_run
 
     def run(self, commands, nthreads):
         results = {}
-        running = []
+        running = {}
         for key in commands:
-            results[key] = self.client.containers.run(self.image, commands[key],
+            results[key] = {}
+            # sleep while we have reached max threads and all running
+            while len(running) >= nthreads and all([running[k].status == 'running' for k in running]):
+                sleep(0.25)
+            # clean out nonrunning containers
+            for k in running:
+                if running[k].status != 'running':
+                    results[k]['exit_status'] = running[k].status
+                    results[k]['log'] = running[k].logs(stdout = True, stderr = True)
+                    running[k].remove(v = True, force = True)
+                    running.pop(k, None)
+            # add a new container
+            assert len(running) < nthreads
+            try:
+                if not self.dry_run:
+                    ctr = self.client.containers.run(self.image, commands[key],
                                                           detach = True, 
                                                           remove = False,
                                                           stderr = True,
@@ -23,103 +34,22 @@ class Docker(object):
                                                           mem_limit = '2g',
                                                           volumes = {'local_repo_path' : {'bind': '/home/jupyter', 'mode': 'rw'}}
                                                           )
-            ctrs = self.client.containers.list()
-    def get(self):
-        pass
-
-    def is_done(self):
-        pass
-
-    def container_cap(self):
-        pass
-    
-
-
-
-def check_needs_manual_grading(course, anm, stu, grader):
-  gradebook_file = os.path.join(course['course_storage_path'], 
-                                      grader,
-                                      course['instructor_repo_path'],
-                                      course['gradebook_filename'])
-  gb = Gradebook('sqlite:///'+gradebook_file)
-
-  try:
-    subm = gb.find_submission(anm, course['student_name_prefix']+stu)
-    flag = subm.needs_manual_grade
-  except MissingEntry as e:
-    print(e)
-  finally:
-    gb.close()
-
-  return flag
-
-def upload_grade(course, anm, stu, grader):
-  print('Getting grade for student ' + stu + ' assignment ' + anm)
-  course_dir_path = os.path.join(course['course_storage_path'], 
-                                      grader,
-                                      course['instructor_repo_path'])
-
-  gradebook_file = os.path.join(course['course_storage_path'], 
-                                      grader,
-                                      course['instructor_repo_path'],
-                                      course['gradebook_filename'])
-  gb = Gradebook('sqlite:///'+gradebook_file)
-
-  try:
-    subm = gb.find_submission(anm, course['student_name_prefix']+stu)
-    score = subm.score
-  except MissingEntry as e:
-    print(e)
-  finally:
-    gb.close()
-  max_score = compute_max_score(course, anm, grader)
-
-  print('Student ' + stu + ' assignment ' + anm + ' score: ' + str(score))
-  print('Assignment ' + anm + ' max score: ' + str(max_score))
-  print('Pct Score: ' + str(100*score/max_score))
-  print('Posting to canvas...')
-  canvas.post_grade(course, anm, stu, str(100*score/max_score))
-
-def compute_max_score(course, anm, grader):
-  #for some incredibly annoying reason, nbgrader refuses to compute a max_score for anything (so we cannot easily convert scores to percentages)
-  #let's compute the max_score from the notebook manually then....
-
-  release_notebook_file = os.path.join(course['course_storage_path'], 
-                                      grader,
-                                      course['instructor_release_path'],
-                                      anm,
-                                      anm+'.ipynb')
-  f = open(release_notebook_file, 'r')
-  parsed_json = json.load(f)
-  f.close()
-  pts = 0
-  for cell in parsed_json['cells']:
-    try:
-      pts += cell['metadata']['nbgrader']['points']
-    except Exception as e:
-      #will throw exception if cells dont exist / not right type -- that's fine, it'll happen a lot.
-      pass
-  return pts
-
-
-
-
-       
-        ##create the local path 
-        #local_repo_path = os.path.join(course['course_storage_path'], 
-        #                                    grader,
-        #                                    course['instructor_repo_path'])
-
-        #print('Running a docker container to check if assignment ' + anm + ' exists for ' + grader)
-        #docker_command = 'docker run --rm -v ' + local_repo_path +':/home/jupyter ubcdsci/r-dsci-grading nbgrader db assignment list'
-        #print(docker_command)
-        #result = str(subprocess.check_output(docker_command.split(' ')))
-        #print('Result:')
-        #print(result)
-        #if anm not in result:
-        #  print('Need to generate assignment ' + anm + ' for grader ' + grader)
-        #  docker_command = 'docker run --rm -v ' + local_repo_path +':/home/jupyter ubcdsci/r-dsci-grading nbgrader generate_assignment --force ' + anm
-        #  print(docker_command)
-        #  subprocess.check_output(docker_command.split(' '))
-
-
+                    running[key] = ctr
+                else:
+                    print('[Dry Run: would have started docker container ' + key + ' with command: ' + commands[key] + ']')
+                    results[key]['exit_status'] = 'dry_run'
+                    results[key]['log'] = 'dry_run'
+            except docker.errors.APIError as e:
+                print('Docker APIError exception encountered when starting docker container: ' + str(commands[key]))
+                results[key]['exit_status'] = 'never_started'
+                results[key]['log'] = str(e)
+            except docker.errors.ImageNotFound as e:
+                print('Docker ImageNotFound exception encountered when starting docker container: ' + str(commands[key]))
+                results[key]['exit_status'] = 'never_started'
+                results[key]['log'] = str(e)
+            except Exception as e:
+                print('Unknown exception encountered when starting docker container: ' + str(commands[key]))
+                results[key]['exit_status'] = 'never_started'
+                results[key]['log'] = str(e)
+            
+        return results
--- rudaux/rudaux/submission.py
+++ rudaux/rudaux/submission.py
@@ -90,3 +90,91 @@ def remove_duplicate_grade_ids(course, anm, stu, grader):
   f.close()
 
 
+
+def check_needs_manual_grading(course, anm, stu, grader):
+  gradebook_file = os.path.join(course['course_storage_path'], 
+                                      grader,
+                                      course['instructor_repo_path'],
+                                      course['gradebook_filename'])
+  gb = Gradebook('sqlite:///'+gradebook_file)
+
+  try:
+    subm = gb.find_submission(anm, course['student_name_prefix']+stu)
+    flag = subm.needs_manual_grade
+  except MissingEntry as e:
+    print(e)
+  finally:
+    gb.close()
+
+  return flag
+
+def upload_grade(course, anm, stu, grader):
+  print('Getting grade for student ' + stu + ' assignment ' + anm)
+  course_dir_path = os.path.join(course['course_storage_path'], 
+                                      grader,
+                                      course['instructor_repo_path'])
+
+  gradebook_file = os.path.join(course['course_storage_path'], 
+                                      grader,
+                                      course['instructor_repo_path'],
+                                      course['gradebook_filename'])
+  gb = Gradebook('sqlite:///'+gradebook_file)
+
+  try:
+    subm = gb.find_submission(anm, course['student_name_prefix']+stu)
+    score = subm.score
+  except MissingEntry as e:
+    print(e)
+  finally:
+    gb.close()
+  max_score = compute_max_score(course, anm, grader)
+
+  print('Student ' + stu + ' assignment ' + anm + ' score: ' + str(score))
+  print('Assignment ' + anm + ' max score: ' + str(max_score))
+  print('Pct Score: ' + str(100*score/max_score))
+  print('Posting to canvas...')
+  canvas.post_grade(course, anm, stu, str(100*score/max_score))
+
+def compute_max_score(course, anm, grader):
+  #for some incredibly annoying reason, nbgrader refuses to compute a max_score for anything (so we cannot easily convert scores to percentages)
+  #let's compute the max_score from the notebook manually then....
+
+  release_notebook_file = os.path.join(course['course_storage_path'], 
+                                      grader,
+                                      course['instructor_release_path'],
+                                      anm,
+                                      anm+'.ipynb')
+  f = open(release_notebook_file, 'r')
+  parsed_json = json.load(f)
+  f.close()
+  pts = 0
+  for cell in parsed_json['cells']:
+    try:
+      pts += cell['metadata']['nbgrader']['points']
+    except Exception as e:
+      #will throw exception if cells dont exist / not right type -- that's fine, it'll happen a lot.
+      pass
+  return pts
+
+
+
+
+       
+        ##create the local path 
+        #local_repo_path = os.path.join(course['course_storage_path'], 
+        #                                    grader,
+        #                                    course['instructor_repo_path'])
+
+        #print('Running a docker container to check if assignment ' + anm + ' exists for ' + grader)
+        #docker_command = 'docker run --rm -v ' + local_repo_path +':/home/jupyter ubcdsci/r-dsci-grading nbgrader db assignment list'
+        #print(docker_command)
+        #result = str(subprocess.check_output(docker_command.split(' ')))
+        #print('Result:')
+        #print(result)
+        #if anm not in result:
+        #  print('Need to generate assignment ' + anm + ' for grader ' + grader)
+        #  docker_command = 'docker run --rm -v ' + local_repo_path +':/home/jupyter ubcdsci/r-dsci-grading nbgrader generate_assignment --force ' + anm
+        #  print(docker_command)
+        #  subprocess.check_output(docker_command.split(' '))
+
+
