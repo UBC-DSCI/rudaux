4754801f377e311fe9b13d4bd93c03e55e3587b0
Trevor Campbell <trevor.d.campbell@gmail.com>
2020-09-11 15:21:40 -0700

    improved docker pool more; modularized container running

--- rudaux/rudaux/docker.py
+++ rudaux/rudaux/docker.py
@@ -16,18 +16,11 @@ class Docker(object):
         return key
 
     def run(self, command):
-        result = {}
-        ctr = self.client.containers.run(self.image, commands[key],
-                                                          detach = False, 
-                                                          remove = False,
-                                                          stderr = True,
-                                                          stdout = True,
-                                                          mem_limit = '2g',
-                                                          volumes = {'local_repo_path' : {'bind': '/home/jupyter', 'mode': 'rw'}}
-                                                          )
-        result['exit_status'] = ctr.status
-        result['log'] = ctr.logs(stdout = True, stderr = True)
-        ctr.remove(v = True, force = True)
+        ctr, result = self._run_container(self.commands[key], False)
+        if ctr:
+            result['exit_status'] = ctr.status
+            result['log'] = ctr.logs(stdout = True, stderr = True)
+            ctr.remove(v = True, force = True)
         return result
 
     def run_all(self, nthreads):
@@ -47,38 +40,47 @@ class Docker(object):
                     running.pop(k, None)
             # add a new container
             assert len(running) < nthreads
-            try:
-                if not self.dry_run:
-                    ctr = self.client.containers.run(self.image, self.commands[key],
-                                                          detach = True, 
-                                                          remove = False,
-                                                          stderr = True,
-                                                          stdout = True,
-                                                          mem_limit = '2g',
-                                                          volumes = {'local_repo_path' : {'bind': '/home/jupyter', 'mode': 'rw'}}
-                                                          )
-                    running[key] = ctr
-                else:
-                    print('[Dry Run: would have started docker container ' + key + ' with command: ' + commands[key] + ']')
-                    results[key]['exit_status'] = 'dry_run'
-                    results[key]['log'] = 'dry_run'
-            except docker.errors.APIError as e:
-                print('Docker APIError exception encountered when starting docker container: ' + str(commands[key]))
-                results[key]['exit_status'] = 'never_started'
-                results[key]['log'] = str(e)
-            except docker.errors.ImageNotFound as e:
-                print('Docker ImageNotFound exception encountered when starting docker container: ' + str(commands[key]))
-                results[key]['exit_status'] = 'never_started'
-                results[key]['log'] = str(e)
-            except Exception as e:
-                print('Unknown exception encountered when starting docker container: ' + str(commands[key]))
-                results[key]['exit_status'] = 'never_started'
-                results[key]['log'] = str(e)
-
+            ctr, results[key] = self._run_container(self.commands[key], True)
+            if ctr:
+                running[key] = ctr
         # clear the commands queue when done
         self.commands = {} 
 
         return results
+
+    def _run_container(self, command, detach):
+        ctr = None
+        result = {}
+        try:
+            if not self.dry_run:
+                ctr = self.client.containers.run(self.image, command,
+                                                      detach = detach, 
+                                                      remove = False,
+                                                      stderr = True,
+                                                      stdout = True,
+                                                      mem_limit = '2g',
+                                                      volumes = {'local_repo_path' : {'bind': '/home/jupyter', 'mode': 'rw'}}
+                                                      )
+            else:
+                print('[Dry Run: would have started docker container ' + key + ' with command: ' + commands[key] + ']')
+                result['exit_status'] = 'dry_run'
+                result['log'] = 'dry_run'
+        except docker.errors.APIError as e:
+            print('Docker APIError exception encountered when starting docker container: ' + str(commands[key]))
+            result['exit_status'] = 'never_started'
+            result['log'] = str(e)
+            ctr = None
+        except docker.errors.ImageNotFound as e:
+            print('Docker ImageNotFound exception encountered when starting docker container: ' + str(commands[key]))
+            result['exit_status'] = 'never_started'
+            result['log'] = str(e)
+            ctr = None
+        except Exception as e:
+            print('Unknown exception encountered when starting docker container: ' + str(commands[key]))
+            result['exit_status'] = 'never_started'
+            result['log'] = str(e) 
+            ctr = None
+        return ctr, result
 
 
 
