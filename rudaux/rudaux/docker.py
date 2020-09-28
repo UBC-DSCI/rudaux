fe2a17ee27da92a5a728f760b45ebe311bd4345e
Trevor Campbell <trevor.d.campbell@gmail.com>
2020-09-27 22:35:58 -0700

    added n_tries to _run_container...

--- rudaux/rudaux/docker.py
+++ rudaux/rudaux/docker.py
@@ -32,7 +32,6 @@ class Docker(object):
                 ctr.reload()
             result['exit_status'] = ctr.status
             result['log'] = ctr.logs(stdout = True, stderr = True).decode('utf-8')
-            #ctr.remove(force = True)
             ctr.remove()
         return result
 
@@ -65,14 +64,12 @@ class Docker(object):
                 if running[k].status not in self.runsts:
                     results[k]['exit_status'] = running[k].status
                     results[k]['log'] = running[k].logs(stdout = True, stderr = True).decode('utf-8')
-                    #running[k].remove(force = True)
                     running[k].remove()
                     to_pop.append(k)
             for k in to_pop:
                 running.pop(k, None)
 
             # add a new container if there are any remaining
-            time.sleep(0.25) #sleep for a 1/4 second for stability when starting containers...?
             if len(job_keys) > 0:
                 assert len(running) < self.n_threads
                 key = job_keys.pop()
@@ -87,46 +84,53 @@ class Docker(object):
 
         return results
 
-    def _run_container(self, command, homedir):
+    def _run_container(self, command, homedir, n_tries = 5):
         ctr = None
         result = {}
-        #TODO don't do this here, but maybe in the external caller: email instructor when errors?
-        try:
-            if not self.dry_run:
-                ctr = self.client.containers.run(self.image, command,
-                                                      detach = True,
-                                                      remove = False,
-                                                      stderr = True,
-                                                      stdout = True,
-                                                      mem_limit = self.mem_per_thread,
-                                                      volumes = {homedir : {'bind': '/home/jupyter', 'mode': 'rw'}} if homedir else {}
-                                                      )
-            else:
-                print('[Dry Run: would have started docker container with command: ' + command + ']')
-                result['exit_status'] = 'dry_run'
-                result['log'] = 'dry_run'
-        except docker.errors.APIError as e:
-            print('Docker APIError exception encountered when starting docker container')
-            print('Command: ' + command)
-            print('Homedir: ' + homedir)
-            result['exit_status'] = 'never_started'
-            result['log'] = 'ERROR: Docker APIError, ' + str(e)
-            ctr = None
-        except docker.errors.ImageNotFound as e:
-            print('Docker ImageNotFound exception encountered when starting docker container')
-            print('Command: ' + command)
-            print('Homedir: ' + homedir)
-            result['exit_status'] = 'never_started'
-            result['log'] = 'ERROR: Docker ImageNotFound, ' + str(e)
-            ctr = None
-        except Exception as e:
-            print('Unknown exception encountered when starting docker container')
-            print('Command: ' + command)
-            print('Homedir: ' + homedir)
-            result['exit_status'] = 'never_started'
-            result['log'] = 'ERROR: Unknown exception, ' + str(e) 
-            ctr = None
+        while ctr is None and n_tries > 0:
+            n_tries -= 1
+            try:
+                if not self.dry_run:
+                    ctr = self.client.containers.run(self.image, command,
+                                                          detach = True,
+                                                          remove = False,
+                                                          stderr = True,
+                                                          stdout = True,
+                                                          mem_limit = self.mem_per_thread,
+                                                          volumes = {homedir : {'bind': '/home/jupyter', 'mode': 'rw'}} if homedir else {}
+                                                          )
+                else:
+                    print('[Dry Run: would have started docker container with command: ' + command + ']')
+                    result['exit_status'] = 'dry_run'
+                    result['log'] = 'dry_run'
+            except docker.errors.APIError as e:
+                print('Docker APIError exception encountered when starting docker container')
+                print('Command: ' + command)
+                print('Homedir: ' + homedir)
+                result['exit_status'] = 'never_started'
+                result['log'] = 'ERROR: Docker APIError, ' + str(e)
+                ctr = None
+                time.sleep(1.)
+                if n_tries > 0:
+                    print('Attempting again; ' + str(n_tries) + ' attempts remaining.')
+            except docker.errors.ImageNotFound as e:
+                print('Docker ImageNotFound exception encountered when starting docker container')
+                print('Command: ' + command)
+                print('Homedir: ' + homedir)
+                result['exit_status'] = 'never_started'
+                result['log'] = 'ERROR: Docker ImageNotFound, ' + str(e)
+                ctr = None
+                time.sleep(1.)
+                if n_tries > 0:
+                    print('Attempting again; ' + str(n_tries) + ' attempts remaining.')
+            except Exception as e:
+                print('Unknown exception encountered when starting docker container')
+                print('Command: ' + command)
+                print('Homedir: ' + homedir)
+                result['exit_status'] = 'never_started'
+                result['log'] = 'ERROR: Unknown exception, ' + str(e) 
+                ctr = None
+                time.sleep(1.)
+                if n_tries > 0:
+                    print('Attempting again; ' + str(n_tries) + ' attempts remaining.')
         return ctr, result
-
-
-
