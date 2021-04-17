3fa1c9691fd3c66f3b0ac091aa46f23a55123878
Trevor Campbell <trevor.d.campbell@gmail.com>
2021-04-16 19:25:37 -0700

    WIP simple flow run example

--- rudaux/rudaux/flow.py
+++ rudaux/rudaux/flow.py
@@ -1,9 +1,11 @@
 import sys, os
+import prefect
 from prefect import Flow
 from prefect.schedules import IntervalSchedule
 from prefect.executors import DaskExecutor
 from traitlets.config import Config
 from traitlets.config.loader import PyFileConfigLoader
+import pendulum as plm
 
 from .course_api import canvas as api
 #from .snapshot import zfs_over_ssh as snap
@@ -19,30 +21,27 @@ def run(args):
               specify a directory with a valid rudaux_config.py file. 
               """
             )
-    __config = Config()
-    __config.merge(PyFileConfigLoader('rudaux_config.py', path=course_dir).load_config())
+    _config = Config()
+    _config.merge(PyFileConfigLoader('rudaux_config.py', path=args.directory).load_config())
 
-    print("Ensuring the prefect server and agent are running")
-    # TODO
+    prefect.context.get("logger").info("Constructing the flow...")
+    schedule = IntervalSchedule(start_date = plm.now('UTC').add(seconds=1),
+                                interval = plm.duration(minutes=args.grade_interval))
+    with Flow("rudaux-grading", schedule = schedule) as flow:
+        logger = prefect.context.get("logger")
+        logger.info('')
+        config = api.validate_config(_config)
+        #subms = api.get_submissions(config, asgn)
+        #asgns = api.get_assignments(config, typeid)
+        #studs = api.get_students(config)
+        #tas = api.get_tas(config)
+        #insts = api.get_instructors(config)
+        #grps = api.get_groups(config)
+        cinfo = api.get_course_info(config)
+        logger.info(str(cinfo))
 
-    print("Constructing the prefect flow...")
-    ## register the rudaux grading flow
-    #schedule = IntervalSchedule(start_date = datetime.utcnow() + timedelta(seconds=1),
-    #                            interval = timedelta(minutes=interval))
-    #with Flow("rudaux-grading", schedule=schedule) as flow:
-    #    logger = prefect.context.get("logger")
-    #    __config
-    #    config = api.validate_config(_config)
-    #    #subms = api.get_submissions(config, asgn)
-    #    #asgns = api.get_assignments(config, typeid)
-    #    #studs = api.get_students(config)
-    #    #tas = api.get_tas(config)
-    #    #insts = api.get_instructors(config)
-    #    #grps = api.get_groups(config)
-    #    cinfo = api.get_course_info(config)
-    #    logger.info(str(cinfo))
-    #print("Registering the flow and executing...")
-    #executor = DaskExecutor(address="tcp://localhost:8786")
-    #flow.register()
-    #flow.run(executor=executor)
+    prefect.context.get("logger").info("Registering the flow and executing...")
+    executor = DaskExecutor(address="tcp://localhost:8786")
+    flow.register("temp project")
+    flow.run(executor=executor)
 
