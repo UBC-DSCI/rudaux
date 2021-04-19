6911c73b7656f621823e44c659b7a44f3d8ae5b7
Trevor Campbell <trevor.d.campbell@gmail.com>
2021-04-18 17:59:30 -0700

    moved run docker into util

--- rudaux/rudaux/grader/git_nbgrader.py
+++ rudaux/rudaux/grader/git_nbgrader.py
@@ -8,9 +8,7 @@ from dictauth.users import add_user, remove_user, get_users
 from collections import namedtuple
 import git
 import shutil
-import docker
-import time
-
+from ..util import _run_docker
 def _recursive_chown(path, uid):
     for root, dirs, files in os.walk(path):  
         for di in dirs:  
@@ -20,65 +18,6 @@ def _recursive_chown(path, uid):
 
 def _clean_jhub_uname(s):
     return ''.join(ch for ch in s if ch.isalnum())
-
-def _run_docker(config, command, homedir = None):
-    ctr = None
-    result = {}
-    n_tries = 5
-    # try to start the container a few times 
-    while ctr is None and n_tries > 0:
-        n_tries -= 1
-        try:
-            #start the container
-            ctr = self.client.containers.run(config.grading_docker_image, command,
-                                                detach = True,
-                                                remove = False,
-                                                stderr = True,
-                                                stdout = True,
-                                                mem_limit = config.grading_docker_memory,
-                                                volumes = {homedir : {'bind': config.grading_docker_bind_folder, 'mode': 'rw'}} if homedir else {}
-                                             )
-        except docker.errors.APIError as e:
-            if n_tries == 0:
-                print('Docker APIError exception encountered when starting docker container')
-                print('Command: ' + command)
-                print('Homedir: ' + homedir)
-            result['exit_status'] = 'never_started'
-            result['log'] = 'ERROR: Docker APIError, ' + str(e)
-            ctr = None
-            time.sleep(10.)
-            if n_tries > 0:
-                print('Failed to start container. Attempting again; ' + str(n_tries) + ' attempts remaining.')
-        except docker.errors.ImageNotFound as e:
-            if n_tries == 0:
-                print('Docker ImageNotFound exception encountered when starting docker container')
-                print('Command: ' + command)
-                print('Homedir: ' + homedir)
-            result['exit_status'] = 'never_started'
-            result['log'] = 'ERROR: Docker ImageNotFound, ' + str(e)
-            ctr = None
-            time.sleep(10.)
-            if n_tries > 0:
-                print('Failed to start container. Attempting again; ' + str(n_tries) + ' attempts remaining.')
-        except Exception as e:
-            if n_tries == 0:
-                print('Unknown exception encountered when starting docker container')
-                print('Command: ' + command)
-                print('Homedir: ' + homedir)
-            result['exit_status'] = 'never_started'
-            result['log'] = 'ERROR: Unknown exception, ' + str(e) 
-            ctr = None
-            time.sleep(10.)
-            if n_tries > 0:
-                print('Failed to start container. Attempting again; ' + str(n_tries) + ' attempts remaining.')
-    if ctr:
-        while ctr.status in ['running', 'created']:
-            time.sleep(0.25)
-            ctr.reload()
-        result['exit_status'] = ctr.status
-        result['log'] = ctr.logs(stdout = True, stderr = True).decode('utf-8')
-        ctr.remove()
-    return result
 
 @task
 def validate_config(config):
--- rudaux/rudaux/util.py
+++ rudaux/rudaux/util.py
@@ -1,5 +1,7 @@
 from prefect import Flow, unmapped, flatten, task
 from prefect.engine import signals
+import docker
+import time
 
 @task
 def build_submission_triplet(assignments, students, submission):
@@ -37,3 +39,65 @@ def reduce_override_pairs(override_create_remove_pairs):
 @task
 def combine_dictionaries(dicts):
     return {k : v for d in dicts for k, v in d.items()}
+
+
+def _run_docker(config, command, homedir = None):
+    ctr = None
+    result = {}
+    n_tries = 5
+    # try to start the container a few times 
+    while ctr is None and n_tries > 0:
+        n_tries -= 1
+        try:
+            #start the container
+            ctr = self.client.containers.run(config.grading_docker_image, command,
+                                                detach = True,
+                                                remove = False,
+                                                stderr = True,
+                                                stdout = True,
+                                                mem_limit = config.grading_docker_memory,
+                                                volumes = {homedir : {'bind': config.grading_docker_bind_folder, 'mode': 'rw'}} if homedir else {}
+                                             )
+        except docker.errors.APIError as e:
+            if n_tries == 0:
+                print('Docker APIError exception encountered when starting docker container')
+                print('Command: ' + command)
+                print('Homedir: ' + homedir)
+            result['exit_status'] = 'never_started'
+            result['log'] = 'ERROR: Docker APIError, ' + str(e)
+            ctr = None
+            time.sleep(10.)
+            if n_tries > 0:
+                print('Failed to start container. Attempting again; ' + str(n_tries) + ' attempts remaining.')
+        except docker.errors.ImageNotFound as e:
+            if n_tries == 0:
+                print('Docker ImageNotFound exception encountered when starting docker container')
+                print('Command: ' + command)
+                print('Homedir: ' + homedir)
+            result['exit_status'] = 'never_started'
+            result['log'] = 'ERROR: Docker ImageNotFound, ' + str(e)
+            ctr = None
+            time.sleep(10.)
+            if n_tries > 0:
+                print('Failed to start container. Attempting again; ' + str(n_tries) + ' attempts remaining.')
+        except Exception as e:
+            if n_tries == 0:
+                print('Unknown exception encountered when starting docker container')
+                print('Command: ' + command)
+                print('Homedir: ' + homedir)
+            result['exit_status'] = 'never_started'
+            result['log'] = 'ERROR: Unknown exception, ' + str(e) 
+            ctr = None
+            time.sleep(10.)
+            if n_tries > 0:
+                print('Failed to start container. Attempting again; ' + str(n_tries) + ' attempts remaining.')
+    if ctr:
+        while ctr.status in ['running', 'created']:
+            time.sleep(0.25)
+            ctr.reload()
+        result['exit_status'] = ctr.status
+        result['log'] = ctr.logs(stdout = True, stderr = True).decode('utf-8')
+        ctr.remove()
+    return result
+
+
