7a6022e918ddf0541785d15b9541383d31da7e04
Trevor Campbell <trevor.d.campbell@gmail.com>
2021-09-04 15:44:36 -0700

    minor import errors in dictauth

--- dictauth/dictauth/__init__.py
+++ dictauth/dictauth/__init__.py
@@ -1,3 +1,3 @@
-from .users import add_user, remove_user, list_users, rename_user
+from .users import add_user, remove_user, list_users, rename_user, make_admin, un_admin
 from .encrypt_password import encrypt_password
 from .dictionary_authenticator import DictionaryAuthenticator
