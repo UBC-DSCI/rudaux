b29ccd7ab50a080c15226129c1b4c6bb72ca6ba2
Trevor Campbell <trevor.d.campbell@gmail.com>
2021-09-12 19:04:59 -0700

    added grader module

---
+++ rudaux/rudaux/grader/grader.py
@@ -0,0 +1,22 @@
+from traitlets.config.configurable import Configurable
+
+class Grader(Configurable):
+    def __init__(self):
+        pass
+
+    def initialize(self):
+        raise NotImplementedError 
+
+    def generate_solution(self):
+        raise NotImplementedError 
+
+    def generate_feedback(self):
+        raise NotImplementedError 
+
+    def get_needs_manual_grading(self):
+        raise NotImplementedError 
+ 
+    def autograde(self):
+        raise NotImplementedError 
+ 
+
