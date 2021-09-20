224f3b56d83e7a328c51393859060efff99ef6a2
Trevor Campbell <trevor.d.campbell@gmail.com>
2021-09-20 14:27:05 -0700

    removed dictauth; split into its own repo

--- dictauth/dictauth/__init__.py
+++
@@ -1,4 +0,0 @@
-from .users import add_user, remove_user, list_users, rename_user, make_admin, un_admin
-from .encrypt_password import encrypt_password
-from .dictionary_authenticator import DictionaryAuthenticator
-from .version import __version__
