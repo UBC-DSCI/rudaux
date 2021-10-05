cdfbc7624e8735a76e3c6e5e9bfd358419d369aa
Trevor Campbell <trevor.d.campbell@gmail.com>
2021-10-05 13:11:52 -0700

    made low level container func separate from prefect

--- rudaux/generalized/container.py
+++ rudaux/generalized/container.py
@@ -1,52 +1,45 @@
 import docker
 import time
 from .utilities import get_logger
-from prefect.engine import signals
 
-def run_container(config, command, homedir = None):
+def run_container(command, docker_image, docker_memory = '2g', work_dir = None, ctr_bind_dir = None, n_tries = 5):
     client = docker.from_env()
     logger = get_logger()
     ctr = None
     result = {}
-    n_tries = 5
-    # try to start the container a few times 
+    # try to start the container a few times
     while ctr is None and n_tries > 0:
         n_tries -= 1
         try:
             #start the container
-            ctr = client.containers.run(config.docker_image, command,
+            ctr = client.containers.run(docker_image, command,
                                                 detach = True,
                                                 remove = False,
                                                 stderr = True,
                                                 stdout = True,
                                                 mem_limit = config.docker_memory,
-                                                volumes = {homedir : {'bind': config.docker_bind_folder, 'mode': 'rw'}} if homedir else {}
+                                                volumes = {work_dir : {'bind': ctr_bind_dir, 'mode': 'rw'}} if (work_dir and ctr_bind_dir) else {}
                                              )
         except docker.errors.APIError as e:
             if n_tries == 0:
-                raise signals.FAIL(f"Docker APIError exception encountered when starting docker container. command {command} homedir {homedir}. error message {str(e)}")
+                raise Exception(f"Docker APIError exception encountered when starting docker container. command {command} work_dir {work_dir}. error message {str(e)}")
             ctr = None
             time.sleep(10.)
             if n_tries > 0:
-                logger.info(f"Docker APIError exception encountered when starting docker container. command {command} homedir {homedir}. error message {str(e)}")
+                logger.info(f"Docker APIError exception encountered when starting docker container. command {command} work_dir {work_dir}. error message {str(e)}")
                 logger.info(f"Failed to start container. Attempting again; {n_tries} attempts remaining.")
         except docker.errors.ImageNotFound as e:
-            if n_tries == 0:
-                raise signals.FAIL(f"Docker ImageNotFound exception encountered when starting docker container. command {command} homedir {homedir}. error message {str(e)}")
             ctr = None
-            time.sleep(10.)
-            if n_tries > 0:
-                logger.info(f"Docker ImageNotFound exception encountered when starting docker container. command {command} homedir {homedir}. error message {str(e)}")
-                logger.info(f"Failed to start container. Attempting again; {n_tries} attempts remaining.")
+            raise Exception(f"Docker ImageNotFound exception encountered when starting docker container. image {image_name} command {command} work_dir {work_dir}. error message {str(e)}")
         except Exception as e:
             if n_tries == 0:
-                raise signals.FAIL(f"Docker unknown exception encountered when starting docker container. command {command} homedir {homedir}. error message {str(e)}")
+                raise Exception(f"Docker unknown exception encountered when starting docker container. command {command} work_dir {work_dir}. error message {str(e)}")
             ctr = None
             time.sleep(10.)
             if n_tries > 0:
-                logger.info(f"Docker unknown exception encountered when starting docker container. command {command} homedir {homedir}. error message {str(e)}")
+                logger.info(f"Docker unknown exception encountered when starting docker container. command {command} work_dir {work_dir}. error message {str(e)}")
                 logger.info(f"Failed to start container. Attempting again; {n_tries} attempts remaining.")
-    
+
     # if the container started successfully, poll until it is finished
     if ctr:
         while ctr.status in ['running', 'created']:
@@ -55,7 +48,7 @@ def run_container(config, command, homedir = None):
         result['exit_status'] = ctr.status
         result['log'] = ctr.logs(stdout = True, stderr = True).decode('utf-8')
         ctr.remove()
-  
+
     # return the result
     return result
 
