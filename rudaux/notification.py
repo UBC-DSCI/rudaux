e83d4386a206b03d55a19c3c5537cb2a2ff73f1a
Trevor Campbell <trevor.d.campbell@gmail.com>
2021-10-29 17:52:56 +0000

    printing notification messages to logs even non non-not days

--- rudaux/notification.py
+++ rudaux/notification.py
@@ -79,23 +79,23 @@ def validate_config(config):
 @task(checkpoint=False)
 def notify(config, grading_notifications, posting_notifications):
     logger = get_logger()
+    sm = SendMail(config)
+    flat_posting_notifications = list(set([item for sublist in posting_notifications for item in sublist]))
+    if len(flat_posting_notifications)>0:
+        logger.info(f"There are grades to post. Creating notification for instructor user ({config.instructor_user})")
+        msg = '\r\n'.join(['You have grades to post!', '-------------------'] + [ f"{note[0]}: {note[1]}" for note in flat_posting_notifications])
+        sm.submit(config.instructor_user, msg)
+    for notification_set in grading_notifications:
+        for grader in notification_set:
+            for grd in notification_set[grader]:
+                logger.info(f"TA user {grader} has {notification_set[grader][grd]['count']} left to grade for assignment {notification_set[grader][grd]['assignment']} in account {grd}.")
+                msg = '\r\n'.join([ f"Grading Assignment---" +
+                                    f"Account: {grd} -- "+
+                                    f"Assignment: {notification_set[grader][grd]['assignment']} -- "+
+                                    f"# Remaining to Grade: {notification_set[grader][grd]['count']}" for grd in notification_set[grader]])
+                sm.submit(grader, msg)
     if plm.now().in_timezone(config.notify_timezone).format('dddd') in config.notify_days:
-        logger.info(f"Today ({plm.now().in_timezone(config.notify_timezone).format('dddd')}) is a notification day")
-        sm = SendMail(config)
-        flat_posting_notifications = list(set([item for sublist in posting_notifications for item in sublist]))
-        if len(flat_posting_notifications)>0:
-            logger.info(f"There are grades to post. Notifying instructor user ({config.instructor_user})")
-            msg = '\r\n'.join(['You have grades to post!', '-------------------'] + [ f"{note[0]}: {note[1]}" for note in flat_posting_notifications])
-            sm.submit(config.instructor_user, msg)
-        for notification_set in grading_notifications:
-            for grader in notification_set:
-                for grd in notification_set[grader]:
-                    logger.info(f"TA user {grader} has {notification_set[grader][grd]['count']} left to grade for assignment {notification_set[grader][grd]['assignment']} in account {grd}.")
-                    msg = '\r\n'.join([ f"Grading Assignment---" +
-                                        f"Account: {grd} -- "+
-                                        f"Assignment: {notification_set[grader][grd]['assignment']} -- "+
-                                        f"# Remaining to Grade: {notification_set[grader][grd]['count']}" for grd in notification_set[grader]])
-                    sm.submit(grader, msg)
+        logger.info(f"Today ({plm.now().in_timezone(config.notify_timezone).format('dddd')}) is a notification day; sending emails")
         sm.notify_all()
 
 #class SMTP(Notification):
