0efc33d38e3cab3c2394f067da7a6f4da9725654
Alireza Iranpour <alireza1iranpour@gmail.com>
2022-09-24 14:22:58 -0700

    Refactored

--- rudaux/model/settings.py
+++ rudaux/model/settings.py
@@ -2,24 +2,24 @@ from typing import Dict, List
 from pydantic import BaseModel, validator
 import importlib
 
+
 class Settings(BaseModel):
     # various constants
-    prefect_queue_name : str = "rudaux-queue"
-    prefect_deployment_prefix : str = "rudaux-deployment-"
-    autoext_prefix : str = "autoext-"
-    autoext_cron_string : str = "1,31 * * * *"
-    snap_prefix : str = "snap-"
-    snap_cron_string : str = "1,16,31,46 * * * *"
-    grade_prefix : str = "grade-"
-    grade_cron_string : str = "0 23 * * *"
-    soln_prefix : str = "soln-"
-    soln_cron_string : str = "30 23 * * *"
-    fdbk_prefix : str = "fdbk-"
-    fdbk_cron_string : str = "45 23 * * *"
+    prefect_queue_name: str = "rudaux-queue"
+    prefect_deployment_prefix: str = "rudaux-deployment-"
+    autoext_prefix: str = "autoext-"
+    autoext_cron_string: str = "1,31 * * * *"
+    snap_prefix: str = "snap-"
+    snap_cron_string: str = "1,16,31,46 * * * *"
+    grade_prefix: str = "grade-"
+    grade_cron_string: str = "0 23 * * *"
+    soln_prefix: str = "soln-"
+    soln_cron_string: str = "30 23 * * *"
+    fdbk_prefix: str = "fdbk-"
+    fdbk_cron_string: str = "45 23 * * *"
     # map of course_group to list of course names in that group
-    course_groups : Dict[str, List[str]]
+    course_groups: Dict[str, List[str]]
     # maps course_group to LMS, GMS, SMS class type
-    lms_classes : Dict[str, str]
-    gs_classes : Dict[str, str]
-    ss_classes : Dict[str, str]
-    
+    lms_classes: Dict[str, str]
+    gs_classes: Dict[str, str]
+    ss_classes: Dict[str, str]
