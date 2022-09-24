aa8f26e6c886e356589c0c4392846ef0bd230a3e
Alireza Iranpour <alireza1iranpour@gmail.com>
2022-09-24 14:21:31 -0700

    Replaced prefect logger with logging module to avoid errors for now

--- rudaux/util/util.py
+++ rudaux/util/util.py
@@ -2,9 +2,15 @@ import prefect
 import os
 import pwd
 import grp
+from prefect import get_run_logger
+import logging
+
 
 def get_logger():
-    return prefect.context.get("logger")
+    # return prefect.context.get("logger")
+    # return get_run_logger()
+    return logging.getLogger("my-logger")
+
 
 def recursive_chown(path, user, group):
     uid = pwd.getpwnam(user).pw_uid
@@ -12,8 +18,6 @@ def recursive_chown(path, user, group):
     os.chown(path, uid, gid)
     for root, dirs, files in os.walk(path):
         for di in dirs:
-          os.chown(os.path.join(root, di), uid, gid)
+            os.chown(os.path.join(root, di), uid, gid)
         for fi in files:
-          os.chown(os.path.join(root, fi), uid, gid)
-
-
+            os.chown(os.path.join(root, fi), uid, gid)
